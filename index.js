a7c18e2a64c62e2157e77776b9a4c910e84a590f
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-08-12 17:55:39 -0600

    syntax edit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 const cors = require('cors');
-let allowedOrigins = *; // allows all domains to make requests to the API
+let allowedOrigins = [*]; // allows all domains to make requests to the API
 app.use(cors({
   origin: (origin, callback) => {
     if(!origin) return callback(null, true);
