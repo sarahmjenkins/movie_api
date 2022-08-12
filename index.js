f185f72e50bce31f4b854ce37ac3789f43b7e202
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-08-12 16:35:48 -0600

    Removed unneeded functions

--- index.js
+++ index.js
@@ -195,30 +195,6 @@ app.delete('/users/:username', passport.authenticate('jwt', {session: false}), (
     });
 });
 
-// Return a list of all users
-// app.get('/users', (req, res) => {
-//   Users.find()
-//     .then((users) => {
-//       res.status(201).json(users);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).send('Error: ' + err);
-//     });
-// });
-
-// Return a user by username
-// app.get('/users/:username', (req, res) => {
-//   Users.findOne({username: req.params.username})
-//     .then((user) => {
-//       res.json(user);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).send('Error: ' + err);
-//     });
-// });
-
 // error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
