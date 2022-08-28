69480d3d6af205bc7925cb5d5837ff216e613664
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-08-28 13:13:23 -0600

    Removed authentication to allow React to access API

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/', (req, res) => {
 });
 
 // Return a list of all movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req,res) => {
+app.get('/movies', (req,res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
