dd7e3ed90b187f12f3a8012d8d2ecaeecc8a9d7f
sarahmjenkins <sarahmjenkins88@gmail.com>
2022-09-08 17:54:04 -0600

    Allow to display user info by username

--- src/index.js
+++ src/index.js
@@ -146,6 +146,18 @@ app.post('/register',
       })
     });
 
+// Return user info by username
+app.get('/users/:username', passport.authenticate('jwt', {session: false}), (req, res) =>{
+  Users.findOne({username: req.params.username})
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 // Allow users to update their user info by username
 app.put('/users/:username',
   [
